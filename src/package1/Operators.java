f378c66720647e3f9a0bd51c9c811b41da0c9ec9
yash19970 <yash_19@outlook.com>
2017-09-08 00:54:43 +0530

    operators and loops

--- src/package1/Operators.java
+++ src/package1/Operators.java
@@ -6,10 +6,29 @@ public class Operators {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int x,y,z;
+		/*int x,y,z;
 		x = 20; y = 30;
 		z = x+y;
 		System.out.println("Answer:"+z);
+		*/
+		/*int x = 100;
+		switch(x){
+			case 10:
+				System.out.println("v.good");
+				break;
+			case 20:
+				System.out.println("good");
+				break;
+			default:
+				System.out.println("bad");
+				break;
+				
+		}*/
+		int x=10;
+		while(x>0){
+			System.out.println(x);
+			x--;
+		}
 	}
 	
 }
