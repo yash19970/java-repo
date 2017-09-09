611196a367a6733ebd6c7e71d78928f5037aa7aa
yash19970 <yash_19@outlook.com>
2017-09-09 14:19:08 +0530

    classes

---
+++ src/package1/ArrayClass.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class ArrayClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int[] arr = {1,3};
+		System.out.println(arr[1]);
+		/*for(int ele : arr){
+			System.out.println(ele);
+		}*/
+		DefC obj1  = new DefC();
+		obj1.id = 1;
+		System.out.print(obj1.id);
+	}
+
+}
